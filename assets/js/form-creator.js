531b58dbeab8c7948484450a02bd4aefc5e397ef
Evan Stoddard (RIT Student) <eds2083@rit.edu>
2023-07-25 11:59:32 -0400

    Input object setter switch statement

--- assets/js/form-creator.js
+++ assets/js/form-creator.js
@@ -1,56 +1,86 @@
-        //Get all the form input select buttons
-        //Each button has a value representing the input type
-        const buttons = document.getElementById("input-select-button-group").querySelectorAll('*');
+//Get all the form input select buttons
+//Each button has a value representing the input type
+const buttons = document.getElementById("input-select-button-group").querySelectorAll('*');
 
-        //Get Form Preview Container
-        //This is where representations of the form inputs added will go
-        const formPreview = document.getElementById("form-preview");
+//Get Form Preview Container
+//This is where representations of the form inputs added will go
+const formPreview = document.getElementById("form-preview");
 
-        function createInputPreview(inputElement) {
-            return `
-            <div class="input-preview input-preview-${inputType}">
-                <input type="${inputType}" name="label-
-            </div>
-            `;
-        }
 
-        function createInputElement(inputType) {
+//Function to handle input select button click
+function handleInputSelectClick(event) {
+    const value = event.target.value;
+    console.log(`Button "${value}" was pressed.`);
+    
+    //Check for form inputs that are not "input" element types
 
-        }
+    var inputObj;
 
-        function createButtonElement() {
-            const newElement = document.createElement('input')
-            newElement.setAttribute('type', 'button');
-            newElement.setAttribute('value', '');
-            newElement.setAttribute('class', 'custom-form_button');
-            return newElement;
-        }
+    switch (value) {
+        case "button":
+            inputObj = new ButtonInput();
+            break;
+        case "checkbox":
+            inputObj = new CheckboxGroupInput();
+            break;
+        case "radio":
+            inputObj = new RadioGroupInput();
+            break;
+        case "select":
+            inputObj = new SelectGroupInput();
+            break;
+        case "text":
+            inputObj = new TextInput();
+            break;
+        case "textarea":
+            inputObj = new TextAreaInput();
+            break;
+        case "number":
+            inputObj = new NumberInput();
+            break;
+        case "range":
+            inputObj = new RangeInput();
+            break;
+        case "color":
+            inputObj = new ColorInput();
+            break;
+        case "date":
+            inputObj = new DateInput();
+            break;
+        case "datetime-local":
+            inputObj = new DateTimeLocalInput();
+            break;
+        case "month":
+            inputObj = new MonthInput();
+            break;
+        case "week":
+            inputObj = new WeekInput();
+            break;
+        case "time":
+            inputObj = new TimeInput();
+            break;
+        case "email":
+            inputObj = new EmailInput();
+            break;
+        case "phone":
+            inputObj = new PhoneInput();
+            break;
+        case "file":
+            inputObj = new FileInput();
+            break;
+        case "url":
+            inputObj = new UrlInput();
+            break;
+        case "image":
+            inputObj = new ImageInput();
+            break;
+        case "hidden":
+            inputObj = new HiddenInput();
+            break;
+    }
+}
 
-        function createCheckboxElement() {
-            const newElement = document.createElement('input')
-            newElement.setAttribute('type', 'checkbox');
-            newElement.setAttribute('value', '');
-            newElement.setAttribute('name', '');
-            newElement.setAttribute('id', '');
-            newElement.setAttribute('class', 'custom-form_checkbox');
-            return newElement;
-        }
-
-        //Function to handle input select button click
-        function handleInputSelectClick(event) {
-            const value = event.target.value;
-            console.log(`Button "${value}" was pressed.`);
-            
-            //Check for form inputs that are not "input" element types
-
-            if (value == "select") {
-
-            } else if (value == "textarea") {
-
-            }
-        }
-
-        //Add a click event listener to each button
-        buttons.forEach((button) => {
-            button.addEventListener("click", handleInputSelectClick);
-        });
+//Add a click event listener to each button
+buttons.forEach((button) => {
+    button.addEventListener("click", handleInputSelectClick);
+});
