266cff3e1cf46416b9268c0b33087ccc5e7ba9f2
Evan Stoddard (RIT Student) <eds2083@rit.edu>
2023-08-07 16:02:36 -0400

    Save Form Data functionality Saves form as html and json array

---
+++ includes/form-editor-endpoint.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Save the form's html representation and list of inputs with the post metadata
+ */
+function save_formdata($post_id) {
+	if ( array_key_exists('form_html', $_POST) ) {
+		update_post_meta(
+			$post_id,
+			'_form_html',
+			$_POST['form_html']
+		);
+	}
+    if ( array_key_exists('form_array', $_POST) ) {
+		update_post_meta(
+			$post_id,
+			'_form_array',
+			$_POST['form_array']
+		);
+	}
+}
+add_action( 'save_post', 'wporg_save_postdata' );
